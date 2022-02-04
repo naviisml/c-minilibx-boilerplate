a9c22a3ed5b4a3c06f27358db20e1048db1dc5c5
Navi Ismail <mailvoornavi@gmail.com>
2022-02-04 18:20:23 +0100

    Update ft_example.c

--- srcs/ft_example.c
+++ srcs/ft_example.c
@@ -6,7 +6,7 @@
 /*   By: nismail <nismail@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/23 00:26:25 by nismail       #+#    #+#                 */
-/*   Updated: 2022/02/04 16:43:19 by nismail       ########   odam.nl         */
+/*   Updated: 2022/02/04 16:44:17 by nismail       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ int	main(void)
 
 	server_initialize(&example);
 	mlx_hook(example.window, 2, (1L << 17), events_loop, &example);
-	mlx_loop_hook(example.mlx, render, &example);
 	mlx_hook(example.window, 17, 1L << 17, game_destroy, &example);
+	mlx_loop_hook(example.mlx, render, &example);
 	mlx_loop(example.mlx);
 	return (0);
 }
